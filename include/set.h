99cdd49dd8ebbc20b0c4dfc0dae972bbc952265f
Alexander Wilson <acwilson96@gmail.com>
2019-02-01 02:22:56 +0000

    Very basic implementation of set.

---
+++ include/set.h
@@ -0,0 +1,132 @@
+#pragma once
+
+#ifndef ATL_SET_H
+#define ATL_SET_H
+
+namespace atl {
+
+template <typename T> class set {
+
+public:
+  /* Constructor */
+  set<T>() : elements_size(0), elements_used(0), initialised(false) {}
+
+  /* Copy Constructor */
+  set<T>(const set<T> &rhs) {
+    if (this == &rhs)
+      return;
+    allocate(rhs.capacity());
+    for (int i = 0; i < rhs.elements_used; ++i)
+      push_back(rhs[i]);
+  }
+
+  /* Assignment Operator */
+  set<T> &operator=(const set<T> &rhs) {
+    if (this == &rhs)
+      return *this;
+    allocate(rhs.capacity());
+    for (int i = 0; i < rhs.size(); ++i)
+      push_back(rhs[i]);
+    return *this;
+  }
+
+  /* Move Constructor */
+  set<T>(const set<T> &&rhs) {
+    if (this == &rhs)
+      return *this;
+    allocate(rhs.capacity());
+    for (int i = 0; i < rhs.elements_used; ++i)
+      push_back(rhs[i]);
+    return *this;
+  }
+
+  /* Move-Assignment Operator */
+  set<T> &operator=(const set<T> &&rhs) {
+    if (this == &rhs)
+      return *this;
+    allocate(rhs.capacity());
+    for (int i = 0; i < rhs.size(); ++i)
+      push_back(rhs[i]);
+    return *this;
+  }
+
+  /* Destructor */
+  ~set<T>() {
+    if (initialised)
+      delete[] elements;
+  }
+
+  bool find(const T &elem) {
+    for (int idx = 0; idx < size(); ++idx)
+      if (elements[idx] == elem)
+        return true;
+
+    return false;
+  }
+
+  void insert(const T &elem) {
+    if (!find(elem)) {
+      push_back(elem);
+    }
+  }
+
+  int size() const { return elements_used; }
+
+private:
+  int elements_size;
+  int elements_used;
+  bool initialised;
+  T *elements;
+
+  const T &operator[](const int index) const { return at(index); }
+
+  /* Allocate a new array of size num_elems. Set elements_size. */
+  void allocate(const int num_elems) {
+    elements = new T[num_elems];
+    elements_size = num_elems;
+  }
+
+  T &at(const int index) const {
+    if (index < elements_size) {
+      return elements[index];
+    }
+    throw "atl::vector::at Out of Bounds Exception";
+  }
+
+  int capacity() const { return elements_size; }
+
+  /* Extend the size of elements[] by num_elems. */
+  void extend(const int num_elems) {
+    /* Store the current state of the set. */
+    const int temp_elements_size = elements_size;
+    const int temp_elements_used = elements_used;
+    T *temp_elements = new T[elements_size];
+    for (int i = 0; i < elements_size; ++i)
+      temp_elements[i] = elements[i];
+
+    /* Clear the set state. */
+    delete[] elements;
+    elements_used = 0;
+
+    /* Re-initialise the state of the set using the extension. */
+    allocate(temp_elements_size + num_elems);
+    for (elements_used = 0; elements_used < temp_elements_size; ++elements_used)
+      elements[elements_used] = temp_elements[elements_used];
+  }
+
+  void push_back(const T &elem) {
+    if (!initialised) {
+      allocate(1);
+      initialised = true;
+    }
+    if (elements_used >= elements_size)
+      extend(1);
+
+    elements[elements_used] = elem;
+    ++elements_used;
+  }
+};
+
+} // namespace atl
+
+#endif
---
+++ test/Test_Set.cpp
@@ -0,0 +1,70 @@
+#include "gtest/gtest.h"
+
+#include "../include/set.h"
+#include "../include/string.h"
+
+TEST(SetTest, Constructor) {
+  atl::set<atl::string> atlSet;
+  atl::string elem1("foo");
+  atl::string elem2("bar");
+  ASSERT_FALSE(atlSet.find(elem1));
+  ASSERT_FALSE(atlSet.find(elem2));
+  atlSet.insert(elem1);
+  ASSERT_TRUE(atlSet.find(elem1));
+  ASSERT_FALSE(atlSet.find(elem2));
+  atlSet.insert(elem2);
+  ASSERT_TRUE(atlSet.find(elem1));
+  ASSERT_TRUE(atlSet.find(elem2));
+}
+
+TEST(SetTest, Usage) {
+  atl::set<atl::string> atlSet;
+  atl::string elem1("foo");
+  atl::string elem2("bar");
+  ASSERT_FALSE(atlSet.find(elem1));
+  ASSERT_FALSE(atlSet.find(elem2));
+  atlSet.insert(elem1);
+  ASSERT_TRUE(atlSet.find(elem1));
+  ASSERT_FALSE(atlSet.find(elem2));
+  atlSet.insert(elem2);
+  ASSERT_TRUE(atlSet.find(elem1));
+  ASSERT_TRUE(atlSet.find(elem2));
+}
+
+TEST(SetTest, CopyConstructor) {
+  atl::set<atl::string> atlSet;
+  atl::string elem1("foo");
+  atl::string elem2("bar");
+  ASSERT_FALSE(atlSet.find(elem1));
+  ASSERT_FALSE(atlSet.find(elem2));
+  atlSet.insert(elem1);
+  ASSERT_TRUE(atlSet.find(elem1));
+  ASSERT_FALSE(atlSet.find(elem2));
+  atlSet.insert(elem2);
+  ASSERT_TRUE(atlSet.find(elem1));
+  ASSERT_TRUE(atlSet.find(elem2));
+
+  atl::set<atl::string> atlSet2(atlSet);
+  ASSERT_TRUE(atlSet2.find(elem1));
+  ASSERT_TRUE(atlSet2.find(elem2));
+}
+
+// The fixture for testing class Project1. From google test primer.
+class Test_Set : public ::testing::Test {
+protected:
+  Test_Set() {
+    // You can do set-up work for each test here.
+  }
+
+  // If the constructor and destructor are not enough for setting up
+  // and cleaning up each test, you can define the following methods:
+  virtual void SetUp() {
+    // Code here will be called immediately after the constructor (right
+    // before each test).
+  }
+
+  virtual void TearDown() {
+    // Code here will be called immediately after each test (right
+    // before the destructor).
+  }
+};
