46a0023192a8f9747a4979ab01f11f57410bf1f3
Alexander Wilson <acwilson96@gmail.com>
2019-06-05 21:39:02 +0100

    atl::shared_ptr debugging

--- tools/lldbatl.py
+++ tools/lldbatl.py
@@ -1,6 +1,43 @@
 import lldb
 import lldb.formatters.Logger
 
+
+def atlsharedprtr_SummaryProvider(valobj, dict):
+    return valobj.GetChildMemberWithName('refCount').Dereference().GetValueAsUnsigned()
+
+class atlshared_ptr_SynthProvider:
+
+    def __init__(self, valobj, dict):
+        self.valobj = valobj
+        self.update()
+
+    def num_children(self):
+        return 1
+        
+    def get_child_index(self, name):
+        return 0
+
+    def get_child_at_index(self, index):
+        return self.ptr.Dereference()
+
+    def get_type_from_name(self):
+        import re
+        name = self.valobj.GetType().GetName()
+        res = re.match("^(atl::)?shared_ptr<(.+)>$", name)
+        if res:
+            return res.group(2)
+        res = re.match("^(atl::)?shared_ptr<(.+), \d+>$", name)
+        if res:
+            return res.group(2)
+        return None
+
+    def update(self):
+        self.ptr = self.valobj.GetChildMemberWithName('ptr')
+
+    def has_children(self):
+        return True
+
+
 def atlstring_SummaryProvider(valobj, dict):
     return valobj.GetChildMemberWithName('string_value').GetSummary()
 
@@ -48,6 +85,10 @@ def atlvector_SummaryProvider(valobj, dict):
     return "size={}".format(valobj.GetNumChildren())
 
 def __lldb_init_module(debugger, dict):
+    debugger.HandleCommand('type summary add -F'
+                           'lldbatl.atlsharedprtr_SummaryProvider -e -x "^atl::shared_ptr<.+>$"')
+    debugger.HandleCommand('type synthetic add -l'
+                           'lldbatl.atlshared_ptr_SynthProvider -x "^atl::shared_ptr<.+>$"')
     debugger.HandleCommand('type summary add -F'
                            'lldbatl.atlstring_SummaryProvider atl::string')
     debugger.HandleCommand('type summary add -F'
