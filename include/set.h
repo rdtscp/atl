da76a57c7e05798710ed3dfa6ae5d5a3155770f8
Alexander Wilson <acwilson96@gmail.com>
2020-04-10 19:57:04 +0100

    Simplify set implementation.

--- include/set.h
+++ include/set.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "vector.h"
 #include <iostream>
 namespace atl {
 
@@ -6,7 +7,7 @@ template <typename T> class set {
 
 public:
   /* Constructor */
-  set<T>() : elements_size(0), elements_used(0), initialised(false) {}
+  set<T>() {}
 
   /* Initializer-List Constructor */
   set<T>(std::initializer_list<T> list) {
@@ -18,47 +19,27 @@ public:
   set<T>(const set<T> &rhs) {
     if (this == &rhs)
       return;
-    allocate(rhs.capacity());
-    elements_used = 0;
-    for (unsigned int i = 0; i < rhs.elements_used; ++i)
-      push_back(rhs[i]);
+
+    m_elements.clear();
+    m_elements.reserve(rhs.size());
+    for (unsigned int i = 0; i < rhs.size(); ++i)
+      insert(rhs.m_elements[i]);
   }
 
   /* Assignment Operator */
   set<T> &operator=(const set<T> &rhs) {
     if (this == &rhs)
       return *this;
-    allocate(rhs.capacity());
-    elements_used = 0;
-    for (unsigned int i = 0; i < rhs.size(); ++i)
-      push_back(rhs[i]);
-    return *this;
-  }
 
-  /* Move Constructor */
-  set<T>(const set<T> &&rhs) {
-    if (this == &rhs)
-      return;
-    allocate(rhs.capacity());
-    for (unsigned int i = 0; i < rhs.elements_used; ++i)
-      push_back(rhs[i]);
-  }
-
-  /* Move-Assignment Operator */
-  set<T> &operator=(const set<T> &&rhs) {
-    if (this == &rhs)
-      return *this;
-    allocate(rhs.capacity());
+    m_elements.clear();
+    m_elements.reserve(rhs.size());
     for (unsigned int i = 0; i < rhs.size(); ++i)
-      push_back(rhs[i]);
+      insert(rhs[i]);
     return *this;
   }
 
   /* Destructor */
-  ~set<T>() {
-    if (initialised)
-      delete[] elements;
-  }
+  ~set<T>() {}
 
   bool operator==(const set<T> &rhs) const {
     if (this == &rhs)
@@ -68,7 +49,7 @@ public:
       return false;
 
     for (unsigned int i = 0; i < rhs.size(); ++i)
-      if (at(i) != rhs.at(i))
+      if (m_elements[i] != rhs.m_elements[i])
         return false;
 
     return true;
@@ -78,7 +59,7 @@ public:
 
   bool find(const T &elem) const {
     for (unsigned int idx = 0; idx < size(); ++idx)
-      if (at(idx) == elem)
+      if (m_elements[idx] == elem)
         return true;
 
     return false;
@@ -86,65 +67,15 @@ public:
 
   void insert(const T &elem) {
     if (!find(elem)) {
-      push_back(elem);
+      m_elements.push_back(elem);
     }
   }
 
-  unsigned int size() const { return elements_used; }
+  unsigned int size() const { return m_elements.size(); }
 
 private:
-  unsigned int elements_size;
-  unsigned int elements_used;
-  bool initialised;
-  T *elements;
+  atl::vector<T> m_elements;
 
-  const T &operator[](const int index) const { return at(index); }
-
-  /* Allocate a new array of size num_elems. Set elements_size. */
-  void allocate(const int num_elems) {
-    elements = new T[num_elems];
-    elements_size = num_elems;
-  }
-
-  T &at(const unsigned int index) const {
-    if (index < elements_size) {
-      return elements[index];
-    }
-    printf("%s\n", "atl::set::at Out of Bounds Exception");
-    throw;
-  }
-
-  int capacity() const { return elements_size; }
-
-  /* Extend the size of elements[] by num_elems. */
-  void extend(const int num_elems) {
-    /* Store the current state of the set. */
-    const unsigned int temp_elements_size = elements_size;
-    T *temp_elements = new T[elements_size];
-    for (unsigned int i = 0; i < elements_size; ++i)
-      temp_elements[i] = elements[i];
-
-    /* Clear the set state. */
-    delete[] elements;
-    elements_used = 0;
-
-    /* Re-initialise the state of the set using the extension. */
-    allocate(temp_elements_size + num_elems);
-    for (elements_used = 0; elements_used < temp_elements_size; ++elements_used)
-      elements[elements_used] = temp_elements[elements_used];
-  }
-
-  void push_back(const T &elem) {
-    if (!initialised) {
-      allocate(1);
-      initialised = true;
-    }
-    if (elements_used >= elements_size)
-      extend(1);
-
-    elements[elements_used] = elem;
-    ++elements_used;
-  }
 };
 
 } // namespace atl
