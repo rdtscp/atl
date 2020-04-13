c0504fd6a7a92c9bca39fbf59b2743407f055386
Alexander Wilson <acwilson96@gmail.com>
2020-04-14 00:33:05 +0100

    Clean up implementation and DRY

--- include/string.h
+++ include/string.h
@@ -1,215 +1,178 @@
 #pragma once
 #include <iostream>
+
+#include "swap.h"
+
 namespace atl {
 
 class string {
-
 public:
+  typedef unsigned int uint;
   typedef const char *const_iterator;
   typedef char *iterator;
 
   /* Constructor */
-  string() : m_size(0u) {
-    string_value = new char[1u];
-    *string_value = '\0';
+  string() : m_size(0u), m_value(new char[1]){
+    *m_value = '\0';
   }
 
   /* Constructor */
-  string(const unsigned int count, const char c) : m_size(count) {
-    string_value = new char[m_size + 1u];
-    char *string_value_ptr = string_value;
-    for (unsigned int idx = 0u; idx < m_size; ++idx) {
-      *string_value_ptr = c;
-      ++string_value_ptr;
+  string(const uint count, const char c) : m_size(count), m_value(new char[m_size + 1]) {
+    for (uint idx = 0u; idx < m_size; ++idx) {
+      m_value[idx] = c;
     }
-    *string_value_ptr = '\0';
+    m_value[m_size] = '\0';
   }
 
   /* Constructor */
-  string(const char *string_literal) {
-    m_size = charBufferLength(string_literal);
-    string_value = new char[m_size + 1u];
-    char *string_value_ptr = string_value;
-    for (unsigned int idx = 0u; idx < m_size; ++idx) {
-      *string_value_ptr = *string_literal;
-      ++string_value_ptr;
+  string(const char *string_literal) : m_size(char_buf_len(string_literal)), m_value(new char[m_size + 1]) {
+    for (uint idx = 0u; idx < m_size; ++idx) {
+      m_value[idx] = *string_literal;
       ++string_literal;
     }
-    *string_value_ptr = '\0';
+    m_value[m_size] = '\0';
   }
 
   /* Copy Constructor */
-  string(const string &rhs) {
-    if (&rhs == this)
-      return;
-
-    m_size = rhs.size();
-    string_value = new char[m_size + 1u];
-    char *string_value_ptr = string_value;
-    for (unsigned int idx = 0u; idx < m_size; ++idx) {
-      *string_value_ptr = rhs[idx];
-      ++string_value_ptr;
+  string(const string &rhs) : m_size(rhs.m_size), m_value(new char[m_size + 1]) {
+    for (uint idx = 0u; idx < m_size; ++idx) {
+      m_value[idx] = rhs[idx];
     }
-    *string_value_ptr = '\0';
   }
 
   /* Assignment Operator */
-  string &operator=(const string &rhs) {
-    if (&rhs == this)
-      return *this;
-
-    delete[] string_value;
-
-    m_size = rhs.size();
-    string_value = new char[m_size + 1u];
-    char *string_value_ptr = string_value;
-    for (unsigned int idx = 0u; idx < m_size; ++idx) {
-      *string_value_ptr = rhs[idx];
-      ++string_value_ptr;
-    }
-    *string_value_ptr = '\0';
+  string &operator=(string rhs) {
+    atl::swap(m_size, rhs.m_size);
+    atl::swap(m_value, rhs.m_value);
     return *this;
   }
 
   /* Move Constructor */
-  string(string &&rhs)
-      : m_size(rhs.m_size), string_value(rhs.string_value) {
-    rhs.string_value = nullptr;
-  }
-
-  /* Move-Assignment Operator */
-  string &operator=(string &&rhs) {
-    delete[] string_value;
-
-    m_size = rhs.m_size;
-    string_value = rhs.string_value;
-
-    rhs.m_size = 0u;
-    rhs.string_value = nullptr;
-    return *this;
+  string(string &&rhs) : string() {
+    atl::swap(m_size, rhs.m_size);
+    atl::swap(m_value, rhs.m_value);
   }
 
   /* Destructor */
-  ~string() { delete[] string_value; }
+  ~string() {
+    m_size = 0;
+    delete[] m_value;
+    m_value = nullptr;
+  }
 
-  char &operator[](const unsigned int index) { return this->at(index); }
-
-  const char operator[](const unsigned int index) const {
+  char &operator[](const uint index) {
     return this->at(index);
   }
 
-  string operator+(const char rhs) const {
-    string output(*this);
-    output += rhs;
-    return output;
-  }
-
-  string operator+(const char *rhs) const {
-    string output(*this);
-    output += rhs;
-    return output;
-  }
-
-  string operator+(const string &rhs) const {
-    string output(*this);
-    output += rhs;
-    return output;
+  const char operator[](const uint index) const {
+    return this->at(index);
   }
 
   string &operator+=(const char rhs) {
-    if (rhs == '\0')
-      return *this;
-
-    const unsigned int lhs_size = this->size();
-    const unsigned int rhs_size = 1u;
-    m_size = lhs_size + rhs_size;
-
-    // Allocate memory for the new string.
-    char *new_string_value = new char[m_size + 1u];
-    char *new_string_value_ptr = new_string_value;
-
-    // Copy the LHS.
-    char *lhs_ptr = string_value;
-    for (unsigned int idx = 0u; idx < lhs_size; ++idx) {
-      *new_string_value_ptr = *lhs_ptr;
-      ++lhs_ptr;
-      ++new_string_value_ptr;
-    }
-
-    *new_string_value_ptr = rhs;
-    ++new_string_value_ptr;
-
-    // Null terminate.
-    *new_string_value_ptr = '\0';
-
-    delete[] string_value;
-    string_value = new_string_value;
+    append(string(1, rhs));
     return *this;
   }
 
   string &operator+=(const char *rhs) {
-    const unsigned int lhs_size = this->size();
-    const unsigned int rhs_size = charBufferLength(rhs);
-    m_size = lhs_size + rhs_size;
-
-    // Allocate memory for the new string.
-    char *new_string_value = new char[m_size + 1u];
-    char *new_string_value_ptr = new_string_value;
-
-    // Copy the LHS.
-    const char *lhs_ptr = string_value;
-    for (unsigned int idx = 0u; idx < lhs_size; ++idx) {
-      *new_string_value_ptr = *lhs_ptr;
-      ++lhs_ptr;
-      ++new_string_value_ptr;
-    }
-
-    // Copy the RHS.
-    const char *rhs_ptr = rhs;
-    for (unsigned int idx = 0u; idx < rhs_size; ++idx) {
-      *new_string_value_ptr = *rhs_ptr;
-      ++rhs_ptr;
-      ++new_string_value_ptr;
-    }
-
-    // Null terminate.
-    *new_string_value_ptr = '\0';
-
-    delete[] string_value;
-    string_value = new_string_value;
+    append(rhs);
     return *this;
   }
 
   string &operator+=(const string &rhs) {
-    const unsigned int lhs_size = this->size();
-    const unsigned int rhs_size = rhs.size();
-    m_size = lhs_size + rhs_size;
-
-    // Allocate memory for the new string.
-    char *new_string_value = new char[m_size + 1u];
-    char *new_string_value_ptr = new_string_value;
-
-    // Copy the LHS.
-    const char *lhs_ptr = string_value;
-    for (unsigned int idx = 0u; idx < lhs_size; ++idx) {
-      *new_string_value_ptr = *lhs_ptr;
-      ++lhs_ptr;
-      ++new_string_value_ptr;
-    }
-
-    // Copy the RHS.
-    for (unsigned int idx = 0u; idx < rhs_size; ++idx) {
-      *new_string_value_ptr = rhs[idx];
-      ++new_string_value_ptr;
-    }
-
-    // Null terminate.
-    *new_string_value_ptr = '\0';
-
-    delete[] string_value;
-    string_value = new_string_value;
+    append(rhs);
     return *this;
   }
+
+  bool operator<(const string &rhs) const {
+    if (m_size < rhs.m_size) {
+      return true;
+    }
+
+    if (m_size > rhs.m_size) {
+      return false;
+    }
+
+    for (uint idx = 0u; idx < m_size; ++idx) {
+      const char currLhs = at(idx);
+      const char currRhs = rhs.at(idx);
+      if (static_cast<int>(currLhs) < static_cast<int>(currRhs)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  bool operator>(const string &rhs) const {
+    if (m_size < rhs.m_size) {
+      return false;
+    }
+    if (m_size > rhs.m_size) {
+      return true;
+    }
+
+    for (uint idx = 0u; idx < m_size; ++idx) {
+      const char currLhs = at(idx);
+      const char currRhs = rhs.at(idx);
+      if (static_cast<int>(currLhs) > static_cast<int>(currRhs)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  bool operator<=(const string &rhs) const {
+    if (m_size < rhs.m_size) {
+      return true;
+    }
+    if (m_size > rhs.m_size) {
+      return false;
+    }
+
+    for (uint idx = 0u; idx < m_size; ++idx) {
+      const char currLhs = at(idx);
+      const char currRhs = rhs.at(idx);
+      if (static_cast<int>(currLhs) > static_cast<int>(currRhs)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  bool operator>=(const string &rhs) const {
+    if (m_size < rhs.m_size) {
+      return false;
+    }
+    if (m_size > rhs.m_size) {
+      return true;
+    }
+
+    for (uint idx = 0u; idx < m_size; ++idx) {
+      const char currLhs = at(idx);
+      const char currRhs = rhs.at(idx);
+      if (static_cast<int>(currLhs) < static_cast<int>(currRhs)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  bool operator==(const string &rhs) const {
+    if (m_size != rhs.m_size) {
+      return false;
+    }
+
+    for (uint idx = 0; idx < m_size; ++idx) {
+      if (at(idx) != rhs.at(idx)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  bool operator!=(const string &rhs) const { return !(*this == rhs); }
 
   bool operator<(const char *rhs) const { return *this < string(rhs); }
 
@@ -219,151 +182,80 @@ public:
 
   bool operator>=(const char *rhs) const { return *this >= string(rhs); }
 
-  bool operator<(const string &rhs) const {
-    const unsigned int lhs_size = this->size();
-    const unsigned int rhs_size = rhs.size();
-    if (lhs_size < rhs_size)
-      return true;
-    if (lhs_size > rhs_size)
-      return false;
-
-    char currLhs;
-    char currRhs;
-    for (unsigned int idx = 0u; idx < rhs_size; ++idx) {
-      currLhs = this->at(idx);
-      currRhs = rhs[idx];
-      if (static_cast<int>(currLhs) < static_cast<int>(currRhs)) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  bool operator>(const string &rhs) const {
-    const unsigned int lhs_size = this->size();
-    const unsigned int rhs_size = rhs.size();
-    if (lhs_size < rhs_size)
-      return false;
-    if (lhs_size > rhs_size)
-      return true;
-
-    char currLhs;
-    char currRhs;
-    for (unsigned int idx = 0u; idx < rhs_size; ++idx) {
-      currLhs = this->at(idx);
-      currRhs = rhs[idx];
-      if (static_cast<int>(currLhs) > static_cast<int>(currRhs)) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  bool operator<=(const string &rhs) const {
-    const unsigned int lhs_size = this->size();
-    const unsigned int rhs_size = rhs.size();
-    if (lhs_size < rhs_size)
-      return true;
-    if (lhs_size > rhs_size)
-      return false;
-
-    char currLhs;
-    char currRhs;
-    for (unsigned int idx = 0u; idx < rhs_size; ++idx) {
-      currLhs = this->at(idx);
-      currRhs = rhs[idx];
-      if (static_cast<int>(currLhs) > static_cast<int>(currRhs)) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  bool operator>=(const string &rhs) const {
-    const unsigned int lhs_size = this->size();
-    const unsigned int rhs_size = rhs.size();
-    if (lhs_size < rhs_size)
-      return false;
-    if (lhs_size > rhs_size)
-      return true;
-
-    char currLhs;
-    char currRhs;
-    for (unsigned int idx = 0u; idx < rhs_size; ++idx) {
-      currLhs = this->at(idx);
-      currRhs = rhs[idx];
-      if (static_cast<int>(currLhs) < static_cast<int>(currRhs)) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  bool operator==(const string &rhs) const {
-    if (this->length() != rhs.length())
-      return false;
-    const unsigned int str_len = this->length();
-    char lhsChar;
-    char rhsChar;
-    for (unsigned int charIdx = 0u; charIdx < str_len; ++charIdx) {
-      lhsChar = this->at(charIdx);
-      rhsChar = rhs.at(charIdx);
-      if (lhsChar != rhsChar)
-        return false;
-    }
-    return true;
-  }
-
-  bool operator==(const char *rhs) const {
-    if (this->length() != charBufferLength(rhs))
-      return false;
-    const unsigned int str_len = this->length();
-    char lhsChar;
-    char rhsChar;
-    for (unsigned int charIdx = 0u; charIdx < str_len; ++charIdx) {
-      lhsChar = this->at(charIdx);
-      rhsChar = rhs[charIdx];
-      if (lhsChar != rhsChar)
-        return false;
-    }
-    return true;
-  }
-
-  bool operator!=(const string &rhs) const { return !(*this == rhs); }
+  bool operator==(const char *rhs) const { return *this == string(rhs); }
 
   bool operator!=(const char *rhs) const { return !(*this == rhs); }
 
-  char &at(const unsigned int index) {
+  char &at(const uint index) {
     if (index < this->size()) {
-      return string_value[index];
+      return m_value[index];
     }
     printf("%s\n", "atl::string::at Out of Bounds Exception");
     throw;
   }
 
-  const char &at(const unsigned int index) const {
+  const char &at(const uint index) const {
     if (index < this->size()) {
-      return string_value[index];
+      return m_value[index];
     }
     printf("%s\n", "atl::string::at Out of Bounds Exception");
     throw;
   }
 
-  const_iterator begin() const { return &string_value[0u]; }
+  void append(const string &rhs) {
+    if (rhs.size() == 0) {
+      return;
+    }
+
+    if (rhs.size() == 1 && rhs[0] == '\0') {
+      return;
+    }
+
+    const int new_size = m_size + rhs.m_size;
+
+    // Allocate memory for the new string.
+    char *new_string_value = new char[new_size + 1];
+    char *new_string_value_ptr = new_string_value;
+
+    // Copy the LHS.
+    const char *lhs_ptr = m_value;
+    for (uint idx = 0u; idx < m_size; ++idx) {
+      *new_string_value_ptr = *lhs_ptr;
+      ++lhs_ptr;
+      ++new_string_value_ptr;
+    }
+
+    // Copy the RHS.
+    const char *rhs_ptr = rhs.m_value;
+    for (uint idx = 0u; idx < rhs.m_size; ++idx) {
+      *new_string_value_ptr = *rhs_ptr;
+      ++rhs_ptr;
+      ++new_string_value_ptr;
+    }
+
+    // Null terminate.
+    *new_string_value_ptr = '\0';
+
+    delete[] m_value;
+    m_size = new_size;
+    m_value = new_string_value;
+  }
+
+  const_iterator begin() const { return &m_value[0u]; }
 
   const_iterator end() const {
-    const unsigned int len = this->size();
-    return &string_value[len];
+    const uint len = this->size();
+    return &m_value[len];
   }
 
-  char *c_str() { return string_value; }
+  char *c_str() { return m_value; }
 
-  char *c_str() const { return string_value; }
+  char *c_str() const { return m_value; }
 
   int find(const char searchChar) const {
-    const unsigned int len = this->length();
+    const uint len = this->length();
     char currChar;
-    for (unsigned int idx = 0u; idx < len; ++idx) {
+    for (uint idx = 0u; idx < len; ++idx) {
       currChar = this->at(idx);
       if (currChar == searchChar) {
         return static_cast<int>(idx);
@@ -372,27 +264,46 @@ public:
     return -1;
   }
 
-  unsigned int length() const { return this->size(); }
+  uint length() const { return this->size(); }
 
-  unsigned int size() const { return m_size; }
+  uint size() const { return m_size; }
 
 private:
-  unsigned int m_size;
-  char *string_value;
+  uint m_size;
+  char *m_value;
 
-  static unsigned int charBufferLength(const char *buf) {
-    unsigned int length = 0u;
+  static uint char_buf_len(const char *buf) {
+    uint length = 0u;
     while (*buf != '\0') {
       ++length;
       ++buf;
     }
     return length;
   }
-}; // namespace atl
+};
 
-static atl::string operator+(const char *lhs, const atl::string &rhs) {
-  return atl::string(lhs) + rhs;
+/* operator+ */
+
+static string operator+(string lhs, const char rhs) {
+  lhs += rhs;
+  return lhs;
 }
+
+static string operator+(string lhs, const char *rhs) {
+  lhs += rhs;
+  return lhs;
+}
+
+static string operator+(string lhs, const string &rhs) {
+  lhs += rhs;
+  return lhs;
+}
+
+static string operator+(const char *lhs, const atl::string &rhs) {
+  return string(lhs) + rhs;
+}
+
+/* Misc */
 
 static bool isspace(const char c) {
   return (c == ' ' || c == '\n' || c == '\r' || c == '\t');
@@ -424,10 +335,10 @@ static int stoi(const char *str) {
   return sign * output;
 }
 
-static int stoi(const atl::string &str) { return stoi(str.c_str()); }
+static int stoi(const string &str) { return stoi(str.c_str()); }
 
 /* https://www.geeksforgeeks.org/implement-itoa/ */
-static atl::string to_string(int num) {
+static string to_string(int num) {
   bool isNegative = false;
 
   /* Handle 0 explicitely, otherwise empty string is printed for 0 */
@@ -442,7 +353,7 @@ static atl::string to_string(int num) {
   }
 
   // Process individual digits
-  atl::string reverseDigits;
+  string reverseDigits;
   while (num != 0) {
     unsigned int rem = num % 10;
     const char next_char =
@@ -458,7 +369,7 @@ static atl::string to_string(int num) {
     reverseDigits += '-';
 
   // Reverse the string
-  atl::string output;
+  string output;
   for (int idx = static_cast<int>(reverseDigits.size()) - 1; idx >= 0; --idx) {
     const unsigned int index = static_cast<unsigned int>(idx);
     char currChar = reverseDigits[index];
